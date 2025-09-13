e3fd5b878929204cb834120b5d1b508e7d654710
Tanmay Mishu <tanmaymishu@gmail.com>
2025-09-14 03:21:51 +0600

    Wrap the subquery within a closure

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -47,9 +47,12 @@ class Poll extends Model
             ->addSelect([
                 'own_vote_id' => PollVote::select('id')
                     ->whereColumn('poll_id', 'polls.id')
-                    ->where('ip_address', request()->ip())
-                    ->orWhere(function ($query) {
-                        $query->whereNotNull('user_id')->where('user_id', auth()->id());
+                    ->where(function ($query) {
+                        $query->where('ip_address', request()->ip())
+                            ->orWhere(function ($subQuery) {
+                                $subQuery->whereNotNull('user_id')
+                                    ->where('user_id', auth()->id());
+                            });
                     })
                     ->take(1),
             ])->with('ownVote');
